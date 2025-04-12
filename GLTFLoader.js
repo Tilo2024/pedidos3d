e3e5e7fb2e750d9efb0f8d66546585807ef834a4
Tilo2024 <roberto.teixeira488@gmail.com>
2025-04-12 02:31:31 -0300

    Add files via upload

---
+++ GLTFLoader.js
@@ -0,0 +1,2 @@
+// GLTFLoader.js - versión clásica compatible con GitHub Pages
+// https://cdn.jsdelivr.net/npm/three@0.148.0/examples/js/loaders/GLTFLoader.js
---
+++ OrbitControls.js
@@ -0,0 +1,2 @@
+// OrbitControls.js - versión clásica compatible con GitHub Pages
+// https://cdn.jsdelivr.net/npm/three@0.148.0/examples/js/controls/OrbitControls.js
